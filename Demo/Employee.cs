5be8936613292ace917fd9c57407081d668596f8
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-23 17:35:45 +0200

    Generic Collection Methods [List]

---
+++ Demo/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class EmpName : IComparer<Employee>
+    {
+        public int Compare(Employee? x, Employee? y)
+        {
+            return x?.Name?.CompareTo(y?.Name) ?? (y is null? 0: -1);
+        }
+    }
+    class Employee : IComparable<Employee>
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(int id, string name, decimal salary)
+        {
+            this.ID = id;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Salary: {Salary}";
+        }
+
+        public int CompareTo(Employee? other)
+        {
+            if (other == null) 
+                return 1;
+            return this.ID.CompareTo(other.ID);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -93,7 +93,7 @@ namespace Demo
             ///}
             ///
             ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
-            
+
             ///nums.Add(1);
             ///nums.Add(2);
             ///nums.Add(3);
@@ -102,6 +102,43 @@ namespace Demo
             ///int res = SumOfList(nums);
             ///Console.WriteLine(res);
 
+            #endregion
+
+            #region Generic Collection Methods [List]
+            List<int> nums = new List<int>(5) { 1, 2, 3, 4, 5 };
+            //nums.Insert(0, 100);
+            //nums.InsertRange(1, [101, 102, 103, 10]);
+            //IReadOnlyList<int> ReadOnlyNumbers = nums.AsReadOnly();
+
+            //int index = nums.BinarySearch(10);
+            //Console.WriteLine($"Index = {index}");
+
+            ///List<Employee> emp = new List<Employee>()
+            ///{
+            ///    new Employee(7, "Samer", 7000),
+            ///    new Employee(10, "Ahmed", 5000),
+            ///    new Employee(13, "Mina", 6000)
+            ///};
+            ///
+            ///emp.Sort(new EmpName());
+            ///
+            ///int index = emp.BinarySearch(new Employee() { ID = 7});
+            ///index = emp.BinarySearch(new Employee() { Name = "Samer" }, new EmpName());
+            ///index = emp.BinarySearch(1, 2, new Employee() { Name = "Samer" }, new EmpName());
+            ///Console.WriteLine($"Index: {index}");
+            
+            ///nums.Clear();
+            ///List <long> n = nums.Select<int, long>(N => N).ToList();
+            ///
+            ///int[] arr = new int[7];
+            ///nums.CopyTo(arr);
+            ///
+            ///nums.Reverse();
+            ///
+            ///foreach (int i in arr)
+            ///{
+            ///    Console.WriteLine(i);
+            ///}
             #endregion
         }
     }
