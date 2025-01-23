8421b808ed4d716c16a1254e0c365ff7a77b9c97
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-23 13:22:41 +0200

    Waht are the Collections

---
+++ Demo/Notes/CollectionNotes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Notes
+{
+    internal class CollectionNotes
+    {
+        #region Intro to Collections
+        ///Waht is Collections?
+        ///Collection is an implemented data structres [Stack, Queue, Linked List, (Binary Search Tree, Graph)] in .NET
+
+        ///Collections Versions
+        ///1. Non-Generic Collection based on object
+        ///a. List
+        ///b. Linked List
+        ///c. Stack
+        ///d. Queue
+        ///e. Dictionary 
+        ///f. Hash Set
+        ///2. Generic Collection based on Generics
+        #endregion
+    }
+}
